682b73422eeab975854be216f024850ac8071781
charles kyalo <charleskyalo689@gmail.com>
2020-10-28 09:33:41 +0300

    write more readable function

--- trippleAddFunction.js
+++ trippleAddFunction.js
@@ -4,3 +4,15 @@
 
 const tripleAdd = num1 => num2 => num3 => num1 + num2 + num3;
 console.log(tripleAdd(20)(10)(30));
+
+/* writing a more readable code function */
+
+function tripleAddFunctionRefactored(num1) {
+    return function (num2) {
+        return function (num3) {
+            return num1 + num2 + num3;
+        }
+    }
+}
+
+console.log(tripleAddFunctionRefactored(20)(30)(40)); //returns 90;
